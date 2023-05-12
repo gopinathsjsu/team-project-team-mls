dbfbeb1f09826dc8f7e5417323eb57c394095f9d
r3tro7 <72056013+r3tro7@users.noreply.github.com>
2023-05-12 01:06:57 -0700

    Updated BackendServer

--- src/side-app/backend-server.js
+++ src/side-app/backend-server.js
@@ -32,3 +32,50 @@ app.use((req,res)=>{
  app.listen(8080,()=>{
                 console.log("server listening on port 8080");
            });
+
+// var dbUrl="mongodb+srv://baka:ittop@cluster0-ebc9w.mongodb.net/ecficio?retryWrites=true&w=majority";
+// mc.connect(dbUrl,{useNewUrlParser:true,useUnifiedTopology:true},(err,client)=>{
+//     if(err)
+//     {
+//         console.log("Err in db connect ",err);
+//     }
+//     else{
+//         app.locals.dbObject=client;
+//         console.log(client.db('ecficio'));
+//         console.log('connected to mongodb');
+//         app.listen(process.env.PORT || 8080,()=>{
+//             console.log("server listening on port ",process.env.PORT);
+//         });
+//     }
+// });
+var dbUrl = "mongodb+srv://mounishjuvvadi:abcd@fitnessclub.exdzzg3.mongodb.net/?retryWrites=true&w=majority";
+async function main() {
+
+    const client = new MongoClient(dbUrl);
+
+    try {
+        // Connect to the MongoDB cluster
+         await client.connect()
+         app.locals.dbObject=client;
+        // c.db('finess').collection('users').find().toArray(function(err, docs) {
+        //     console.log(JSON.stringify(docs));
+        // });
+        //console.log(c.db('finess').collection('users').collectionName);
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+
+main().catch(console.error);
+
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
